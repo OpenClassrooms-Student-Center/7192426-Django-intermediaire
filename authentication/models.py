72968e9246f3afd0acd217fec8472dcc0e2abe2d
Rafiq Hilali <rafiq@lambertlabs.com>
2021-04-24 22:15:46 +0100

    P2C1S3: set up the Django app with a custom user model

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    CREATOR = 'CREATOR'
+    SUBSCRIBER = 'SUBSCRIBER'
+
+    ROLE_CHOICES = (
+        (CREATOR, 'Creator'),
+        (SUBSCRIBER, 'Subscriber'),
+    )
+    profile_photo = models.ImageField()
+    role = models.CharField(max_length=30, choices=ROLE_CHOICES)
+    
--- fotoblog/settings.py
+++ fotoblog/settings.py
@@ -125,3 +125,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'authentication.User'
+
