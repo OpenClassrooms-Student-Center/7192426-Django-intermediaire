50214b2e1f673ecfa07dae4d7b32841b7d904eb8
Rafiq Hilali <rafiq@lambertlabs.com>
2021-04-25 03:51:07 +0100

    P3C1S1: added blog and photo models

---
+++ blog/models.py
@@ -0,0 +1,18 @@
+from django.conf import settings
+from django.db import models
+
+
+class Photo(models.Model):
+    image = models.ImageField()
+    caption = models.CharField(max_length=128, blank=True)
+    uploader = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+
+class Blog(models.Model):
+    photo = models.ForeignKey(Photo, null=True, on_delete=models.SET_NULL, blank=True)
+    title = models.CharField(max_length=128)
+    content = models.CharField(max_length=5000)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(auto_now_add=True)
+    starred = models.BooleanField(default=False)
