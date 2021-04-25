71e22af93252680bbbf8c31a82824b7b4f8dd53f
Rafiq Hilali <rafiq@lambertlabs.com>
2021-04-25 22:48:35 +0100

    Added styling to the site

--- fotoblog/settings.py
+++ fotoblog/settings.py
@@ -111,7 +111,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = [BASE_DIR.joinpath('static/')]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
