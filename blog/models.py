3b9ae02d96e623470c5a9abf581e753b7a520869
Rafiq Hilali <rafiq@lambertlabs.com>
2021-04-26 04:12:24 +0100

    P3C4S1: add resize_image method to Photo

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.db import models
+from PIL import Image
 
 
 class Photo(models.Model):
@@ -7,6 +8,13 @@ class Photo(models.Model):
     caption = models.CharField(max_length=128, blank=True, verbose_name='légende')
     uploader = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     date_created = models.DateTimeField(auto_now_add=True)
+
+    IMAGE_MAX_SIZE = (800, 800)
+
+    def resize_image(self):
+        image = Image.open(self.image)
+        image.thumbnail(self.IMAGE_MAX_SIZE)
+        image.save(self.image.path)
 
 
 class Blog(models.Model):
