96febc65e84e79c402c0fc2b736392e08b8638bd
Rafiq Hilali <rafiq@lambertlabs.com>
2021-04-28 02:38:55 +0100

    P4C2S3: added filter to display posted at

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -1,4 +1,9 @@
 from django import template
+from django.utils import timezone
+
+MINUTE = 60
+HOUR = 60 * MINUTE
+DAY = 24 * HOUR
 
 register = template.Library()
 
@@ -6,6 +11,16 @@ register = template.Library()
 @register.filter
 def model_type(value):
     return type(value).__name__
+
+
+@register.filter
+def get_posted_at_display(posted_at):
+    seconds_ago = (timezone.now() - posted_at).total_seconds()
+    if seconds_ago <= HOUR:
+        return f'Posted {int(seconds_ago // MINUTE)} minutes ago.'
+    elif seconds_ago <= DAY:
+        return f'Posted {int(seconds_ago // HOUR)} hours ago.'
+    return f'Posted at {posted_at.strftime("%H:%M %d %b %y")}'
 
 
 @register.simple_tag(takes_context=True)
