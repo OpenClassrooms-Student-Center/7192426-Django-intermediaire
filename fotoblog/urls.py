2e7cec698c74f40c32d6af6b7f74ac902039a612
Rafiq Hilali <rafiq@lambertlabs.com>
2021-04-28 18:11:31 +0100

    P4C2S5: added photo snippet and photo feed

--- fotoblog/urls.py
+++ fotoblog/urls.py
@@ -27,6 +27,7 @@ urlpatterns = [
     path('profile-photo/upload', authentication.views.upload_profile_photo,
          name='upload_profile_photo'),
     path('home/', blog.views.home, name='home'),
+    path('photo-feed/', blog.views.photo_feed, name='photo_feed'),
     path('photo/upload/', blog.views.photo_upload, name='photo_upload'),
     path('blog/create', blog.views.blog_and_photo_upload, name='blog_create'),
     path('blog/<int:blog_id>', blog.views.view_blog, name='view_blog'),
