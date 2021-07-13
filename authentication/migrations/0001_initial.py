9349476bb6b52f078ba19e9bc6eabb0f209aa998
Gérald Gounot <gerald@gounot.eu>
2021-07-13 16:42:20 +0200

    P2C1S3: French translation

--- authentication/migrations/0001_initial.py
+++ authentication/migrations/0001_initial.py
@@ -30,7 +30,7 @@ class Migration(migrations.Migration):
                 ('is_active', models.BooleanField(default=True, help_text='Designates whether this user should be treated as active. Unselect this instead of deleting accounts.', verbose_name='active')),
                 ('date_joined', models.DateTimeField(default=django.utils.timezone.now, verbose_name='date joined')),
                 ('profile_photo', models.ImageField(upload_to='')),
-                ('role', models.CharField(choices=[('CREATOR', 'Creator'), ('SUBSCRIBER', 'Subscriber')], max_length=30)),
+                ('role', models.CharField(choices=[('CREATOR', 'Créateur'), ('SUBSCRIBER', 'Abonné')], max_length=30)),
                 ('groups', models.ManyToManyField(blank=True, help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.', related_name='user_set', related_query_name='user', to='auth.Group', verbose_name='groups')),
                 ('user_permissions', models.ManyToManyField(blank=True, help_text='Specific permissions for this user.', related_name='user_set', related_query_name='user', to='auth.Permission', verbose_name='user permissions')),
             ],
--- authentication/models.py
+++ authentication/models.py
@@ -7,9 +7,9 @@ class User(AbstractUser):
     SUBSCRIBER = 'SUBSCRIBER'
 
     ROLE_CHOICES = (
-        (CREATOR, 'Creator'),
-        (SUBSCRIBER, 'Subscriber'),
+        (CREATOR, 'Créateur'),
+        (SUBSCRIBER, 'Abonné'),
     )
-    profile_photo = models.ImageField()
-    role = models.CharField(max_length=30, choices=ROLE_CHOICES)
+    profile_photo = models.ImageField(verbose_name='photo de profil')
+    role = models.CharField(max_length=30, choices=ROLE_CHOICES, verbose_name='rôle')
     
