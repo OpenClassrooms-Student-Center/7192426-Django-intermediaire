b05bd77a368e79ee4eeb2db475cb82e98dd40abe
Gérald Gounot <gerald@gounot.eu>
2021-07-15 14:51:23 +0200

    P3C1S2: French translation

--- blog/models.py
+++ blog/models.py
@@ -3,16 +3,16 @@ from django.db import models
 
 
 class Photo(models.Model):
-    image = models.ImageField()
-    caption = models.CharField(max_length=128, blank=True)
+    image = models.ImageField(verbose_name='image')
+    caption = models.CharField(max_length=128, blank=True, verbose_name='légende')
     uploader = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     date_created = models.DateTimeField(auto_now_add=True)
 
 
 class Blog(models.Model):
     photo = models.ForeignKey(Photo, null=True, on_delete=models.SET_NULL, blank=True)
-    title = models.CharField(max_length=128)
-    content = models.CharField(max_length=5000)
+    title = models.CharField(max_length=128, verbose_name='titre')
+    content = models.CharField(max_length=5000, verbose_name='contenu')
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     date_created = models.DateTimeField(auto_now_add=True)
     starred = models.BooleanField(default=False)
