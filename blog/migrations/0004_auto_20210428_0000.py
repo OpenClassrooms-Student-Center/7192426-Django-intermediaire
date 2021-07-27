OpenClassrooms-Student-Center/7107341-Intermediate-Django

P3C6S3: added custom migration to move from author to contributors

blog/migrations/0004_auto_20210428_0000.py
@@ -1,0 +1,22 @@
+# Generated by Django 3.2 on 2021-04-28 00:00
+
+from django.db import migrations
+
+
+def migrate_author_to_contributors(apps, schema_editor):
+    Blog = apps.get_model('blog', 'Blog')
+    for blog in Blog.objects.all():
+        if blog.author:
+            blog.contributors.add(
+                blog.author, through_defaults={'contribution': 'Primary Author'})
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('blog', '0003_auto_20210427_2352'),
+    ]
+
+    operations = [
+        migrations.RunPython(migrate_author_to_contributors)
+    ]

OpenClassrooms-Student-Center/7192426-Django-intermediaire

blog/migrations/0004_auto_20210428_0000.py
@@ -1,0 +1,22 @@
+# Generated by Django 3.2 on 2021-04-28 00:00
+
+from django.db import migrations
+
+
+def migrate_author_to_contributors(apps, schema_editor):
+    Blog = apps.get_model('blog', 'Blog')
+    for blog in Blog.objects.all():
+        if blog.author:
+            blog.contributors.add(
+                blog.author, through_defaults={'contribution': 'Auteur principal'})
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('blog', '0003_auto_20210427_2352'),
+    ]
+
+    operations = [
+        migrations.RunPython(migrate_author_to_contributors)
+    ]